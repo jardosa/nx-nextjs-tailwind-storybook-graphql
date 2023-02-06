486ab22dbd2520df5832f5322198aaa51e0c13d3
John Ardosa <jardosa@live.com>
2023-02-06 09:11:29 +0800

    initial commit

--- apps/nextjs-with-tailwind-storybook/pages/_app.tsx
+++ apps/nextjs-with-tailwind-storybook/pages/_app.tsx
@@ -1,17 +1,24 @@
+
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache()
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Head>
         <title>Welcome to nextjs-with-tailwind-storybook!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </ApolloProvider>
   );
 }
 
